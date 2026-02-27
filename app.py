6cfa2422440cdb99a0994c79d9ebb5b8b762c938
24f2007182 <24f2007182@ds.study.iitm.ac.in>
2026-02-27 16:00:29 +0530

    COmpany dashboard set up and routing

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for,request , flash
+import io
+
+from flask import Flask, render_template, redirect, send_file, url_for,request , flash
 from flask_sqlalchemy import SQLAlchemy
 from seed_ata import seed
 from models import db,Student, Company, Admin, JobPosition, Application, User, Placement
@@ -8,6 +10,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placementapp.db'
 app.config['SECRET_KEY'] = 'da1417b94cc6998e458868c66d8d4f85f8bef30762605829'
+UPLOAD_FOLDER = 'static/resumes'
 
 db.init_app(app)
 with app.app_context():
@@ -110,7 +113,6 @@ def showDrives():
     selectedApplication = None
     selectedDrive = None
 
-
     if viewAppId:
         selectedApplication = Application.query.filter_by(applicationId = viewAppId).first()    
     if viewDriveId:
@@ -135,9 +137,10 @@ def showCompanyDash(companyId):
         totalDrives = JobPosition.query.filter_by(companyId = companyId).count()
         applications = []
         placements = []
-        for drive in drives:
-            placement = Placement.query.filter_by(jobId = drive.jobId).first()
+        for drive in drives:            
             application = Application.query.filter_by(jobId = drive.jobId).first()
+            if application != None:
+                placement = Placement.query.filter_by(applicationId = application.applicationId).first()
             applications.append(application)
             placements.append(placement)
         totalApplications = len(applications)
@@ -152,10 +155,10 @@ def createDrive(companyId):
     if request.method == 'POST':
         newJob = JobPosition(companyId = companyId,
                              driveName = request.form['name'],
-                            position = request.form['position'],
+                            positionOpen = request.form['position'],
                             description = request.form['desc'],
-                            skills = request.form['skills'],
-                            experience = request.form['exp'],
+                            skillsRequired = request.form['skills'],
+                            experienceRequired = request.form['exp'],
                             salary = request.form['sal'],
                             location = request.form['location'])
         db.session.add(newJob)
@@ -163,38 +166,81 @@ def createDrive(companyId):
         flash('Added new drive successfully!')
         return redirect(url_for('showCompanyDash', companyId = companyId))
 
-@app.route('/company/<int:companyId>/viewApplications')
-def showCompanyAppplications(companyId):
-    jobId = request.args.get('jobId')
-    if jobId:
-        applications = Application.query.filter_by(jobId = jobId).all()
-    drives = JobPosition.query.filter_by(companyId = companyId).all()
-    for drive in drives:
+@app.route('/company/<int:companyId>/viewApplications', methods = ["GET", "POST"])
+def showCompanyApplications(companyId):
+    
+    if request.method == "GET":
+        viewAppId = request.args.get("viewAppId")
+        selectedApplication = None
+        drives = JobPosition.query.filter_by(companyId = companyId).all()
+
+        
+
+        if viewAppId:
+            selectedApplication = Application.query.filter_by(applicationId = viewAppId).first()   
+        
         applications = []
-        application = Application.query.filter_by(jobId = drive.jobId).first()
-        applications.append(application)
+        for drive in drives:
+            
+            applicationsList = Application.query.filter_by(jobId = drive.jobId).all()
+            for application in applicationsList:
+                applications.append(application)
 
-    return render_template('./company/viewApplications', applications = applications,companyId = companyId )
+        return render_template('./company/viewApplications.html',drives = drives, applications = applications,companyId = companyId, selectedApplication = selectedApplication )
+    
+    if request.method == "POST":
+        jobId = request.form['jobId']
+        statusVal = None
+        statusVal = request.form['statusVal']
+        viewAppId = request.args.get("viewAppId")
+        selectedApplication = None
+        drives = JobPosition.query.filter_by(companyId = companyId).all()
+        applications = []
+        applicationsList = Application.query.filter_by(jobId = jobId).all()
+        for application in applicationsList:
+            if statusVal:
+                if application.status == statusVal:
+                    applications.append(application)
+            else:
+                applications.append(application)
 
-@app.route('/company/<int:companyId>/viewApplications/<int:applicationId>')
+        return render_template('./company/viewApplications.html',drives = drives, applications = applications,companyId = companyId, selectedApplication = selectedApplication )
+
+
+    
+@app.route('/company/<int:companyId>/viewApplications/<int:applicationId>', methods = ["POST"])
 def updateApplicationStatus(companyId, applicationId):
-    updatedStatus = request.args['status']
+    updatedStatus = request.form['status']
     application = Application.query.filter_by(applicationId = applicationId).first_or_404()
     application.status = updatedStatus
     db.session.commit()
-    return redirect(url_for('showApplications', companyId = companyId))
+    return redirect(url_for('showCompanyApplications', companyId = companyId))
+
+@app.route('/company/<int:companyId>/viewDrives')
+def showCompanyDrives(companyId):
+    drives = JobPosition.query.filter_by(companyId = companyId).all()
+    viewDriveId = request.args.get("viewDriveId")
+    selectedDrive = None  
+    if viewDriveId:
+        selectedDrive = JobPosition.query.filter_by(jobId = viewDriveId).first()
+    return render_template('./company/existingDrives.html', companyId = companyId, selectedDrive = selectedDrive, drives = drives)
 
 @app.route('/company/<int:companyId>/closeDrive/<int:jobId>')
 def closeDrive(companyId, jobId):
     job = JobPosition.query.filter_by(jobId = jobId).first_or_404()
     job.active = False
     db.session.commit()
-    return redirect(url_for('showDrives', companyId = companyId ))
+    return redirect(url_for('showCompanyDrives', companyId = companyId ))
+@app.route('/company/<int:companyId>/viewApplications/<int:studentId>')
+def viewResume(companyId,studentId):
+    student = Student.query.get(studentId)
+    return send_file(student.resume, as_attachment=False)
 
 #students login routes 
 @app.route('/student/<int:studentId>/dashboard')
 def showStudentDash(studentId):
     student = Student.query.filter_by(studentId = studentId).first()
     return render_template('./student/dashboard.html', student = student)
+
 if __name__ == "__main__":
     app.run(debug=True)
